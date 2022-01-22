ce9c3a25a546fcf569aeb4130cfd68526be3a213
Goalkee32 <67052974+Goalkee32@users.noreply.github.com>
2022-01-22 18:38:46 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
-html_text = requests.get('https://www.dn.se/sok/?q=Sverigedemokraterna&page=1&sort=newest&date=').text
-soup = BeautifulSoup(html_text, 'lxml')
-nyhet = soup.find_all('a', class_='article-results__item th-nyheter')
-for nyhet in nyhet:
+def hitta_nyheter():
+    html_text = requests.get('https://www.dn.se/sok/?q=Sverigedemokraterna&page=1&sort=newest&date=').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    nyhet = soup.find_all('a', class_='article-results__item th-nyheter')
+    for index, nyhet in enumerate(nyhet):
 
-    nyhet_name = nyhet.find('div', class_='article-results__content').text
-    linker = soup.find("a", {"article-results__item th-nyheter"}).attrs['href']
+        nyhet_name = nyhet.find('div', class_='article-results__content').text
+        linker = soup.find("a", {"article-results__item th-nyheter"}).attrs['href']
+        with open(f'nyheter/{index}.txt', 'w') as f:
+            f.write(f'''
+            Artikel namn: {nyhet_name}
+            Länk till artikel: https://www.dn.se{linker}
+            ''')
+        print(f'Fil sparad: {index}')
 
-    print(f'''
-    Artikel namn: {nyhet_name}
-    Länk till artikel: https://www.dn.se{linker}
-    ''')
+        print('')
 
-    print('')
+if __name__ == '__main__':
+    while True:
+        hitta_nyheter()
+        time_wait = 10
+        print(f'Väntar {time_wait} minuter...')
+        time.sleep(time_wait * 60)
